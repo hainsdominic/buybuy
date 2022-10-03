5d96dead38baae296a9223940be93985c1f6f0c7
Dominic Hains <contact.dominichains@gmail.com>
2022-10-02 20:21:32 -0400

    feat: diesel initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# BuyBuy
+
+## Setup
+
+`diesel setup`
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use buybuy::db::establish_connection;
+
+#[allow(unused_variables, unused_mut)]
+fn main() {
+    let mut connection = establish_connection();
+}
